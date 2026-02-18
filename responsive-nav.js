a07931e4351bea56f6fe3923c9e5fb0ec19258d1
Paul Glasgow <paulglasgow@gmail.com>
2026-02-18 06:03:38 -0700

    testing-gpt

--- responsive-nav.js
+++
@@ -1,7 +0,0 @@
-// Root shim for backward compatibility.
-(function(){
-  const s=document.createElement('script');
-  s.src='js/responsive-nav.js';
-  s.defer=true;
-  document.head.appendChild(s);
-})();
