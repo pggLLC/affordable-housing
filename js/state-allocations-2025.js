f8d07413fbcde47985e19a9786bbe6dc468b1c4e
Paul Glasgow <paulglasgow@gmail.com>
2026-02-18 20:43:58 -0700

    colorado map update

--- js/state-allocations-2025.js
+++
@@ -1,7 +0,0 @@
-// js/state-allocations-2025.js
-// NOTE: Novogradac/Novoco pages linked are behind a (402) paywall when fetched programmatically.
-// This file currently aliases 2026 data so the Year dropdown functions without breaking.
-// Replace with true 2025 values when available.
-(function(){
-  if (window.StateAllocations2026) window.StateAllocations2025 = window.StateAllocations2026;
-})();
